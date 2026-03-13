8cbf293d30b223150e34b3e11e42065b42365d58
unknown <Administrador@ALGUARISA.LOCAL>
2026-03-13 12:33:52 -0400

    cambios de ruta predeterminada de flux

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'inicio')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('home', 'dashboard')->name('web.home');
 });
 
 require __DIR__.'/settings.php';

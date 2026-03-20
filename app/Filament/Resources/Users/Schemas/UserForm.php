64bd9ad1ce7443f2394b26eee35fe56b20e7258e
Antonny Maluenga <gabrielmalu15@gmail.com>
2026-03-20 14:24:57 -0400

    modal create user

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Users\Schemas;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -13,21 +14,28 @@ class UserForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
-                DateTimePicker::make('email_verified_at'),
-                TextInput::make('password')
-                    ->password()
-                    ->required(),
-                Textarea::make('two_factor_secret')
-                    ->columnSpanFull(),
-                Textarea::make('two_factor_recovery_codes')
-                    ->columnSpanFull(),
-                DateTimePicker::make('two_factor_confirmed_at'),
+                Fieldset::make('Datos Básicos')
+                    ->columnSpanFull()
+                    ->columns(2)
+                    ->schema([
+                        TextInput::make('name')
+                            ->label(__('Name'))
+                            ->required(),
+                        TextInput::make('email')
+                            ->label(__('Email'))
+                            ->email()
+                            ->required(),
+                        /*DateTimePicker::make('email_verified_at'),*/
+                        TextInput::make('password')
+                            ->label(__('Password'))
+                            ->password()
+                            ->required(),
+                        /*Textarea::make('two_factor_secret')
+                            ->columnSpanFull(),
+                        Textarea::make('two_factor_recovery_codes')
+                            ->columnSpanFull(),
+                        DateTimePicker::make('two_factor_confirmed_at'),*/
+                    ]),
             ]);
     }
-}
+    }
